6d3b2893d9b6578bce799f12b9353f5565e60c0c
DerZade <DerZade@gmail.com>
2020-10-28 20:17:59 +0100

    Improve obj. icon size

--- src/style/utils/objectIconLayerFactory.js
+++ src/style/utils/objectIconLayerFactory.js
@@ -4,10 +4,13 @@ const { ICON_SIZE_FACTOR } = require('./constants');
  * Create MapboxLayer-Object for generic Arma 3 objects
  * @param {string} name Layer name
  * @param {number} [size] Icon size (default: 24)
+ * @param options
+ * @param {number} options.coefMin 
+ * @param {number} options.coefMax 
  * @param {string} [source] Icon sprite name (if it doesn't match name param)
  * @returns MapboxLayer-Object
  */
-module.exports = function objectIconLayerFactory (name, size = 24, source) {
+module.exports = function objectIconLayerFactory (name, size = 24, { coefMin, coefMax } = { coefMin: 1, coefMax: 1 }, source) {
     return {
         id: name,
         type: 'symbol',
@@ -16,7 +19,11 @@ module.exports = function objectIconLayerFactory (name, size = 24, source) {
         layout: {
             'icon-image': `objects/${source || name}`,
             'icon-anchor': 'center',
-            'icon-size': size * ICON_SIZE_FACTOR
+            'icon-size': [
+                "interpolate", ["linear"], ["zoom"],
+                4, size * ICON_SIZE_FACTOR * coefMin,
+                6, size * ICON_SIZE_FACTOR * coefMax
+            ]
         }
     };
 }
